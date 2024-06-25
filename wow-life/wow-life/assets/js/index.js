f056077ffa9a72a3db376bb1197ece8c0bc524a4
Anderson Xavier <dersoli@gmail.com>
2024-06-25 17:05:20 -0300

    novas alterações feitas

---
+++ wow-life/wow-life/assets/js/index.js
@@ -0,0 +1,131 @@
+jQuery(document).ready(function($){
+    new Glider(document.querySelector('#aulas-recomendadas.secao-1 .aulas'), {
+        slidesToScroll: 1,
+        slidesToShow: 4.2,
+        draggable: true,
+        arrows: {
+          prev: '.glider-prev.secao-1',
+          next: '.glider-next.secao-1'
+        }
+    });
+
+    new Glider(document.querySelector('#exercicios-em-casa.secao-2 .aulas'), {
+        slidesToScroll: 1,
+        slidesToShow: 4.2,
+        draggable: true,
+        arrows: {
+          prev: '.glider-prev.secao-2',
+          next: '.glider-next.secao-2'
+        }
+    });
+
+    new Glider(document.querySelector('#saude-mental.secao-3 .aulas'), {
+        slidesToScroll: 1,
+        slidesToShow: 4.2,
+        draggable: true,
+        arrows: {
+          prev: '.glider-prev.secao-3',
+          next: '.glider-next.secao-3'
+        }
+    });
+
+    new Glider(document.querySelector('#categorias-areas-da-vida.secao-4 .categorias'), {
+        slidesToScroll: 1,
+        slidesToShow: 4.2,
+        draggable: true,
+        arrows: {
+          prev: '.glider-prev.secao-4',
+          next: '.glider-next.secao-4'
+        }
+    });
+
+    new Glider(document.querySelector('#aulas-recomendadas.secao-5 .aulas'), {
+        slidesToScroll: 1,
+        slidesToShow: 4.2,
+        draggable: true,
+        arrows: {
+          prev: '.glider-prev.secao-5',
+          next: '.glider-next.secao-5'
+        }
+    });
+
+    new Glider(document.querySelector('#exercicios-em-casa.secao-6 .aulas'), {
+        slidesToScroll: 1,
+        slidesToShow: 4.2,
+        draggable: true,
+        arrows: {
+          prev: '.glider-prev.secao-6',
+          next: '.glider-next.secao-6'
+        }
+    });
+
+    new Glider(document.querySelector('#saude-mental.secao-7 .aulas'), {
+        slidesToScroll: 1,
+        slidesToShow: 4.2,
+        draggable: true,
+        arrows: {
+          prev: '.glider-prev.secao-7',
+          next: '.glider-next.secao-7'
+        }
+    });
+
+    new Glider(document.querySelector('#aulas-recomendadas.secao-8 .aulas'), {
+        slidesToScroll: 1,
+        slidesToShow: 4.2,
+        draggable: true,
+        arrows: {
+          prev: '.glider-prev.secao-8',
+          next: '.glider-next.secao-8'
+        }
+    });
+
+    new Glider(document.querySelector('#exercicios-em-casa.secao-9 .aulas'), {
+        slidesToScroll: 1,
+        slidesToShow: 4.2,
+        draggable: true,
+        arrows: {
+          prev: '.glider-prev.secao-9',
+          next: '.glider-next.secao-9'
+        }
+    });
+
+    new Glider(document.querySelector('#saude-mental.secao-10 .aulas'), {
+        slidesToScroll: 1,
+        slidesToShow: 4.2,
+        draggable: true,
+        arrows: {
+          prev: '.glider-prev.secao-10',
+          next: '.glider-next.secao-10'
+        }
+    });
+
+    new Glider(document.querySelector('#aulas-recomendadas.secao-11 .aulas'), {
+        slidesToScroll: 1,
+        slidesToShow: 4.2,
+        draggable: true,
+        arrows: {
+          prev: '.glider-prev.secao-11',
+          next: '.glider-next.secao-11'
+        }
+    });
+
+    new Glider(document.querySelector('#exercicios-em-casa.secao-12 .aulas'), {
+        slidesToScroll: 1,
+        slidesToShow: 4.2,
+        draggable: true,
+        arrows: {
+          prev: '.glider-prev.secao-12',
+          next: '.glider-next.secao-12'
+        }
+    });
+
+    new Glider(document.querySelector('#saude-mental.secao-13 .aulas'), {
+        slidesToScroll: 1,
+        slidesToShow: 4.2,
+        draggable: true,
+        arrows: {
+          prev: '.glider-prev.secao-13',
+          next: '.glider-next.secao-13'
+        }
+    });
+})
