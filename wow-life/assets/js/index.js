333627731eeb00a9428194d7c967068864fb291c
Anderson Xavier <dersoli@gmail.com>
2024-06-24 23:01:46 -0300

    home wow life

---
+++ wow-life/assets/js/index.js
@@ -0,0 +1,9 @@
+new Glider(document.querySelector('.aulas'), {
+    slidesToScroll: 1,
+    slidesToShow: 4.5,
+    draggable: true,
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+});
